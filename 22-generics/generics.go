d8e8c311ee470dbda1347a8549bf12f403a39abb
Stephen Mahon <stemahon@gmail.com>
2022-04-20 22:36:04 -0500

    ex 22, 23

---
+++ 22-generics/generics.go
@@ -0,0 +1,25 @@
+package main
+
+// generics from version 1.18. I'm using 1.17.2
+// [ ] come back to this soon
+
+// func MapKeys[K comparable, V any](m map[K]V) []K {
+//     r := make([]K, 0, len(m))
+//     for k := range m {
+//         r = append(r, k)
+//     }
+//     return r
+// }
+
+// type List[T any] struct {
+// 	head, tail *element[T]
+// }
+
+// type element[T any] struct {
+// 	next *element[T]
+// 	val T
+// }
+
+func main() {
+
+}
