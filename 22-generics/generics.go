2596224af6ea42192b7fdcc6082a441c0656fdc7
Stephen Mahon <stemahon@gmail.com>
2022-04-23 21:45:21 -0500

    ex 26 - 30

--- 22-generics/generics.go
+++ 22-generics/generics.go
@@ -3,6 +3,8 @@ package main
 // generics from version 1.18. I'm using 1.17.2
 // [ ] come back to this soon
 
+// import "fmt"
+
 // func MapKeys[K comparable, V any](m map[K]V) []K {
 //     r := make([]K, 0, len(m))
 //     for k := range m {
@@ -12,14 +14,42 @@ package main
 // }
 
 // type List[T any] struct {
-// 	head, tail *element[T]
+//     head, tail *element[T]
 // }
 
 // type element[T any] struct {
-// 	next *element[T]
-// 	val T
+//     next *element[T]
+//     val  T
 // }
 
-func main() {
+// func (lst *List[T]) Push(v T) {
+//     if lst.tail == nil {
+//         lst.head = &element[T]{val: v}
+//         lst.tail = lst.head
+//     } else {
+//         lst.tail.next = &element[T]{val: v}
+//         lst.tail = lst.tail.next
+//     }
+// }
 
-}
+// func (lst *List[T]) GetAll() []T {
+//     var elems []T
+//     for e := lst.head; e != nil; e = e.next {
+//         elems = append(elems, e.val)
+//     }
+//     return elems
+// }
+
+// func main() {
+//     var m = map[int]string{1: "2", 2: "4", 4: "8"}
+
+//     fmt.Println("keys m:", MapKeys(m))
+
+//     _ = MapKeys[int, string](m)
+
+//     lst := List[int]{}
+//     lst.Push(10)
+//     lst.Push(13)
+//     lst.Push(23)
+//     fmt.Println("list:", lst.GetAll())
+// }
