839e64f3c4c05ab1f1a32cbd2cb1e0a4774bee43
Stephen Mahon <stemahon@gmail.com>
2022-04-30 23:22:00 -0500

    ex 38

---
+++ 38-rate-limiting/rate-limiting.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	request := make(chan int, 5)
+	for i := 1; i <= 5; i++ {
+		request <- i
+	}
+	close(request)
+
+	limiter := time.Tick(200 * time.Millisecond)
+
+	for req := range request {
+		<-limiter
+		fmt.Println("request", req, time.Now())
+	}
+
+	burstyLimiter := make(chan time.Time, 3)
+
+	for i := 0; i < 3; i++ {
+		burstyLimiter <- time.Now()
+	}
+
+	go func() {
+		for t := range time.Tick(200 * time.Millisecond) {
+			burstyLimiter <- t
+		}
+	}()
+
+	burstyRequests := make(chan int, 5)
+	for i := 1; i <= 5; i++ {
+		burstyRequests <- i
+	}
+	close(burstyRequests)
+	for req := range burstyRequests {
+		<-burstyLimiter
+		fmt.Println("request", req, time.Now())
+	}
+}
