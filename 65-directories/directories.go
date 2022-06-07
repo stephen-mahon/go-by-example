aa1533943b0db2a28d8aa57c026ef5e9dcfc1415
Stephen Mahon <stemahon@gmail.com>
2022-06-06 22:58:08 -0500

    ex 65

--- 65-directories/directories.go
+++ 65-directories/directories.go
@@ -1,6 +1,10 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
 func check(e error) {
 	if e != nil {
@@ -9,6 +13,7 @@ func check(e error) {
 }
 
 func main() {
+
 	err := os.Mkdir("subdir", 0755)
 	check(err)
 
@@ -20,4 +25,44 @@ func main() {
 	}
 
 	createEmptyFile("subdir/file1")
+
+	err = os.MkdirAll("subdir/parent/child", 0755)
+	check(err)
+
+	createEmptyFile("subdir/parent/file2")
+	createEmptyFile("subdir/parent/file3")
+	createEmptyFile("subdir/parent/child/file4")
+
+	c, err := os.ReadDir("subdir/parent")
+	check(err)
+
+	fmt.Println("Listing subdir/parent")
+	for _, entry := range c {
+		fmt.Println(" ", entry.Name(), entry.IsDir())
+	}
+
+	err = os.Chdir("subdir/parent/child")
+	check(err)
+
+	c, err = os.ReadDir(".")
+	check(err)
+
+	fmt.Println("Listing subdir/parent/child")
+	for _, entry := range c {
+		fmt.Println(" ", entry.Name(), entry.IsDir())
+	}
+
+	err = os.Chdir("../../..")
+	check(err)
+
+	fmt.Println("Visiting subdir")
+	err = filepath.Walk("subdir", visit)
+}
+
+func visit(p string, info os.FileInfo, err error) error {
+	if err != nil {
+		return err
+	}
+	fmt.Println(" ", p, info.IsDir())
+	return nil
 }
